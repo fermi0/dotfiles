e492865f9b103c1c23782b978fcce83dd34e5e90
Saksham <fermi4676.e@gmail.com>
2022-07-29 18:15:52 +0545

    -> added: 2> /dev/null, tail --pid, tmux select-window -> modified: wiki_speak, empty_query, session, not_found, main -> code clean up

--- scripts/wikipedia/wiki.sh
+++ scripts/wikipedia/wiki.sh
@@ -3,7 +3,8 @@
 query_exec () {
     ~/.local/bin/wikit \
         $(python3 ~/Documents/Configs/dotfiles/scripts/wikipedia/query.py) \
-        -a > ~/Documents/Configs/dotfiles/scripts/wikipedia/wiki.txt
+        -a > ~/Documents/Configs/dotfiles/scripts/wikipedia/wiki.txt \
+        2> /dev/null
     echo "[+] Query requested..."
 }
 
@@ -12,52 +13,91 @@ wiki_speak () {
         mpv \
             ~/Documents/Configs/dotfiles/scripts/wikipedia/interesting_wiki.mp3 \
             --speed=1.2
+    else
+        tmux select-window \
+            -t='empty query' \
+            2> /dev/null
     fi
 
-    /usr/local/bin/gtts-cli \
-        -f ~/Documents/Configs/dotfiles/scripts/wikipedia/wiki.txt | \
-        mpv - -speed=1.1 \
-        --force-seekable \
-        --demuxer-seekable-cache=yes \
-        --demuxer-max-bytes=2000MiB \
-        --demuxer-readahead-secs=2000
+    if [ -s wiki.txt ]; then
+        /usr/local/bin/gtts-cli \
+            -f ~/Documents/Configs/dotfiles/scripts/wikipedia/wiki.txt | \
+            mpv - -speed=1.1 \
+            --force-seekable \
+            --demuxer-seekable-cache=yes \
+            --demuxer-max-bytes=2000MiB \
+            --demuxer-readahead-secs=2000 \
+            2> /dev/null
+    else
+        tmux kill-window -t 'wiki text'
+        not_found
+    fi
 }
 
 empty_query () {
-    tmux neww -n 'empty query' -t='wiki speak': vim ~/Documents/Configs/dotfiles/scripts/wikipedia/query.txt
+    if [ ! -s query.txt ]; then
+        tmux neww -n 'empty query' \
+            -t='wiki speak': \
+            vim ~/Documents/Configs/dotfiles/scripts/wikipedia/query.txt
+    fi
+
+    tail \
+        --pid=$(pgrep -a "vim" | grep "query" | cut -d " " -f1) \
+        -f /dev/null \
+        2> /dev/null
 }
 
 session () {
     query_exec
-    wait $(pgrep wikit)
-    sleep 3 && tmux neww -n 'wiki text' -t='wiki speak': less ~/Documents/Configs/dotfiles/scripts/wikipedia/wiki.txt&
+    tail \
+        --pid=$(pgrep wikit) \
+        -f /dev/null \
+        2> /dev/null
+
+    sleep 3 && \
+        tmux neww -n 'wiki text' \
+        -t='wiki speak': \
+        less ~/Documents/Configs/dotfiles/scripts/wikipedia/wiki.txt&
+
     tmux select-window -t='wiki'
     wiki_speak
 }
 
 not_found () {
-    until ! grep -q "Page not found" wiki.txt && [ -s wiki.txt ];
+    # pnf=0
+    until ! grep -q "Page not found" wiki.txt && [ -s wiki.txt ]; # || [ $pnf -le 6 ];
     do
+        empty_query
         echo "[-] Page not found, Requesting again..."
         query_exec
+        # $(( pnf++ ))
     done
+    # if [ $pnf -eq 6 ]; then
+    #     empty_query 
+    #     not_found
+    # fi
 
-    wait $(pgrep wikit)
-    sleep 3 && tmux neww -n 'wiki text' -t='wiki speak': less ~/Documents/Configs/dotfiles/scripts/wikipedia/wiki.txt&
+    tail \
+        --pid=$(pgrep wikit) \
+        -f /dev/null \
+        2> /dev/null
+
+    sleep 3 && \
+        tmux neww -n 'wiki text' \
+        -t='wiki speak': \
+        less ~/Documents/Configs/dotfiles/scripts/wikipedia/wiki.txt&
+
     tmux select-window -t='wiki'
     wiki_speak
 }
 
 main () {
-    if [ ! -s query.txt ]; then
-        empty_query
-        sleep 5
-    fi
+    empty_query
     session
-    if grep -q "Page not found" wiki.txt || [ ! -s wiki.txt ]; then
-        tmux kill-window -t 'wiki text'
-        not_found
-    fi
+    # if grep -q "Page not found" wiki.txt || [ ! -s wiki.txt ]; then
+    #     tmux kill-window -t 'wiki text'
+    #     not_found
+    # fi
 }
 
 main
