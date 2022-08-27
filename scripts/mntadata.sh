b1a33d8090d1e7408a04ccc455982f8031c5aefd
Saksham <fermi4676.e@gmail.com>
2022-08-27 22:59:06 +0545

    [Initial commit]

--- scripts/mntadata.sh
+++ scripts/mntadata.sh
@@ -7,6 +7,7 @@ for i in {b..z};
             sudo mount "/dev/sd${i}1" "/media/oppenheimer/adata"
             echo "/media/oppenheimer/adata mounted!"
             sleep 3;
+            break
         else
             echo "/dev/sd${i}1 does not exist! Retrying..."
             sleep 1;
